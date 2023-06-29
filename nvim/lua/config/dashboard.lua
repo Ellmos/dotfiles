70a5c862ece99e837272cbaf5ccb907bc9c18a57
romain.doulaud <romain.doulaud@epita.fr>
2023-06-29 11:54:43 +0900

    almost done fixing dashboard

--- nvim/lua/config/dashboard.lua
+++ nvim/lua/config/dashboard.lua
@@ -1,35 +1,58 @@
 require("indent_blankline").setup {
     filetype_exclude = { "dashboard" }
 }
+----------------------------------Dashboard-------------------------------
 
---------------------------------Dashboard-------------------------------
 local home = os.getenv('HOME')
-local db = require('dashboard')
 
-db.preview_command = 'cat | lolcat -F 0.3'
-db.preview_file_path = home .. '/.config/nvim/lua/config/startimage.cat'
-db.preview_file_height = 11
-db.preview_file_width = 70
-
-db.custom_center = {
-    {icon = '  ',
-    desc = 'Recently latest session               ',
-    shortcut = 'SPC l',
-    action = 'SessionLoad'},
-
-    {icon = '  ',
-    desc = 'Find  File                            ',
-    action = 'Telescope find_files find_command=rg,--hidden,--files',
-    shortcut = 'SPC f'},
-
-    {icon = '  ',
-    desc = 'Find  word                            ',
-    action = 'Telescope live_grep',
-    shortcut = 'SPC d'},
-
-    {icon = '  ',
-    desc = 'Edit Dotfiles                         ',
-    action = ':e /home/elmos/Documents/dotfiles/',
-    shortcut = 'SPC e'},
+require('dashboard').setup {
+    theme = 'doom',
+    config = {
+        header = {},
+        center = {
+            {
+                icon = '  ',
+                desc = 'Recently latest session               ',
+                action = 'Leaderf mru --popup',
+                key = 'l',
+                icon_hl = 'Title',
+                desc_hl = 'String',
+                key_hl = 'Number',
+            },
+            {
+                icon = '  ',
+                desc = 'Find  File                            ',
+                action = 'Telescope find_files find_command=rg,--hidden,--files',
+                key = 'f',
+                icon_hl = 'Title',
+                desc_hl = 'String',
+                key_hl = 'Number',
+            },
+            {
+                icon = '  ',
+                desc = 'Find  word                            ',
+                action = 'Telescope live_grep',
+                key = 'w',
+                icon_hl = 'Title',
+                desc_hl = 'String',
+                key_hl = 'Number',
+            },
+            {
+                icon = '  ',
+                desc = 'Edit Dotfiles                         ',
+                action = ':e /home/elmos/Documents/dotfiles/',
+                key = 'e',
+                icon_hl = 'Title',
+                desc_hl = 'String',
+                key_hl = 'Number',
+            },
+        }
+    },
+    preview = {
+        command = 'cat | lolcat -F 0.3',
+        file_path = home .. '/.config/nvim/lua/config/startimage.cat',
+        file_height = 13,
+        file_width = 70,
+    },
 }
 
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -6,17 +6,18 @@ return require('packer').startup(function(use)
     use { 'ellisonleao/gruvbox.nvim', commit = 'cb7a8a867cfaa7f0e8ded57eb931da88635e7007' }
 
     -- Dashboard
-    use { 'glepnir/dashboard-nvim' }
-    use { 'lewis6991/gitsigns.nvim' }
+    use { 'glepnir/dashboard-nvim', requires = { 'kyazdani42/nvim-web-devicons' }}
+    --use { 'Shatur/neovim-session-manager' }
 
     -- Lines
-    use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons' } }
+    use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons' }}
     use { 'akinsho/bufferline.nvim', requires = { 'kyazdani42/nvim-web-devicons' }, tag = 'v2.*' }
 
     -- Useful
     use { 'nvim-treesitter/nvim-treesitter', commit = 'd47f3469e3a783e7d1382ab3fe95ba2fa3021ec0' }
     use { 'kyazdani42/nvim-tree.lua', requires = { 'kyazdani42/nvim-web-devicons' }, tag = 'nightly' }
-    use { 'nvim-telescope/telescope.nvim', tag = '0.1.1', requires = {{ 'nvim-lua/plenary.nvim' }}}
+    use { 'nvim-telescope/telescope.nvim', tag = '0.1.1', requires = { 'nvim-lua/plenary.nvim' }}
+    use { 'lewis6991/gitsigns.nvim' }
     use { 'lukas-reineke/indent-blankline.nvim' }
     use { 'windwp/nvim-autopairs' }
     use { 'preservim/nerdcommenter' }
