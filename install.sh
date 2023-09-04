64edd3b985c769a26a7624012a484a2659e52f43
romain <romain.doulaud@epita.fr>
2023-09-05 00:23:10 +0200

    installer script finished

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ PrintBlock() {
     local l=${#str}
     local paddingLength=$(( (lineLength - l) / 2 ))
 
+    echo $'\n\n'
     PrintLine $lineLength
 
     padding=""
@@ -28,6 +29,13 @@ PrintBlock() {
     PrintLine $lineLength
 }
 
+
+
+# Check if program has already been ran
+if [ -e backupDotfiles ]; then 
+    echo "There is already a backup of some dotfiles, if you want to run the program delete the folder 'backupDotfiles'"
+    exit 0
+fi
 
 
 PrintBlock "Updating Packages"
@@ -60,57 +68,81 @@ sudo apt install -y \
 sudo apt autoremove
 
 
-REPO_DIR=$(pwd)
-warnings=""
+DOTFILES_DIR=$(pwd)
+
+# Backup current dotfiles
+mkdir backupDotfiles
 
 # Temp folder for installation 
+rm -rf installer
 mkdir installer
-mkdir backupDotfiles
 cd installer
+
+
+
+
+warnings=$'Some steps might have failed check below for logs:\n'
+
 
 #---------------Fonts
 PrintBlock "Installing fonts"
-if [ ! -d "/usr/local/share/fonts" ]; then
-    sudo mkdir /usr/local/share/fonts
+if ! fc-list | grep -q "HackNerdFont"; then
+  if [ ! -e /usr/local/share/fonts ]; then
+      sudo mkdir -p /usr/local/share/fonts
+  fi
+
+if [ ! -e /usr/local/share/fonts/HackNerdFont ]; then
+      sudo cp -r fonts/HackNerdFont /usr/local/share/fonts/
+      fc-cache
+  else
+      warnings=$warnings$'Could not copy HackNerdFont, folder /usr/local/share/fonts/HackNerdFont already exists\n'
+  fi
 fi
 
-if [ ! -d "/usr/local/share/fonts/HackNerdFont"]; then
-    sudo cp -r fonts/HackNerdFont /usr/local/share/fonts/
-    fc-cache
-else
-    warnings=$warnings"Could not copy HackNerdFont, folder /usr/local/share/fonts/HackNerdFont already exists\n"
-fi
 
 
 
 #---------------Alacritty
 PrintBlock "Installing Alacritty"
-git clone https://github.com/alacritty/alacritty.git
-cd alacritty
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+if ! which rustup &>/dev/null; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
 rustup override set stable
 rustup update stable
 
-sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
-cargo build --release
-cd ..
 
-if [ -d "~/.config/alacritty" ]; then
-    cp -r ~/.config/alacritty backupDotfiles/
+if ! which alacritty &>/dev/null; then
+    git clone https://github.com/alacritty/alacritty.git
+    cd alacritty
+
+    sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+    cargo build --release
+
+    cd ..
+    rm -rf alacritty
 fi
-ln -s "$REPO_DIR/alacritty" ~/.config/
+
+if [ -e ~/.config/alacritty ]; then
+    mv ~/.config/alacritty "$DOTFILES_DIR/backupDotfiles/"
+fi
+ln -s "$DOTFILES_DIR/alacritty" ~/.config/
 
 
 
 #---------------ZSH
 PrintBlock "Installing ZSH"
 sudo apt install zsh
+echo "Enter you password to change the shell to zsh"
 chsh -s $(which zsh)
-ln -s "$REPO_DIR/zsh" ~/.config/
 
-if [ -f "~/.config/.zshrc" ]; then
-    cp ~/.zshrc backupDotfiles/zshrc
+if [ -e ~/.config/zsh ]; then
+    mv ~/.config/zsh "$DOTFILES_DIR/backupDotfiles/"
+fi
+ln -s "$DOTFILES_DIR/zsh" ~/.config/
+
+if [ -e ~/.zshrc ]; then
+    mv ~/.zshrc "$DOTFILES_DIR/backupDotfiles/zshrc"
 fi
 ln -s ~/.config/zsh/.zshrc ~/.zshrc
 
@@ -119,60 +151,67 @@ ln -s ~/.config/zsh/.zshrc ~/.zshrc
 PrintBlock "Installing Polybar"
 sudo apt install polybar
 
-if [ -d "~/.config/polybar" ]; then
-    cp -r ~/.config/polybar backupDotfiles/
+if [ -e ~/.config/polybar ]; then
+    mv ~/.config/polybar "$DOTFILES_DIR/backupDotfiles/"
 fi
-ln -s "$REPO_DIR/polybar" ~/.config/
+ln -s "$DOTFILES_DIR/polybar" ~/.config/
 
 
 #---------------Rofi
 PrintBlock "Installing Rofi"
 sudo apt install rofi
-if [ -d "~/.config/rofi" ]; then
-    cp -r ~/.config/rofi backupDotfiles/
+if [ -e ~/.config/rofi ]; then
+    mv ~/.config/rofi "$DOTFILES_DIR/backupDotfiles/"
 fi
-ln -s "$REPO_DIR/rofi" ~/.config/
+ln -s "$DOTFILES_DIR/rofi" ~/.config/
 
 
 #---------------NeoVim
 PrintBlock "Installing neovim"
-wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
-chmod +x nvim.appimage
 
-if [ ! -d "/opt/nvim"]; then
-    sudo mkdir /opt/nvim
+if ! which nvim &>/dev/null; then
+    wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+    chmod +x nvim.appimage
+
+    if [ ! -e /opt/nvim ]; then
+        sudo mkdir -p /opt/nvim
+    fi
+
+    if  [ ! -e /opt/nvim/nvim.appimage ]; then
+        sudo mv nvim.appimage /opt/nvim
+    else
+        warnings=$warnings$'Could not copy nvim.appimage, /opt/nvim/nvim.appimage already exists\n'
+    fi
+
+    if [ ! -e /usr/bin/nvim ]; then
+        sudo ln -s /opt/nvim/nvim.appimage /usr/bin/nvim
+    else
+        warnings=$warnings$'Could not symlink nvim.appimage to /usr/bin/nvim, /usr/bin/nvim already exists\n'
+        fi
+
+    if [ ! -e /usr/local/bin/nvim ]; then
+        sudo ln -s /opt/nvim/nvim.appimage /usr/local/bin/nvim
+    else
+        warnings=$warnings$'Could not symlink nvim.appimage to /usr/local/bin/nvim, /usr/local/bin/nvim already exists\n'
+    fi
 fi
 
-if [ ! -f "/opt/nvim/nvim.appimage"]; then
-    sudo mv nvim.appimage /opt/nvim
-else
-    warnings=$warnings"Could not copy nvim.appimage, /opt/nvim/nvim.appimage already exists\n"
+if [ -e ~/.config/nvim ]; then
+    mv ~/.config/nvim "$DOTFILES_DIR/backupDotfiles/"
 fi
+ln -s "$DOTFILES_DIR/nvim" ~/.config/
 
-if [ ! -f "/usr/bin/nvim"]; then
-    sudo ln -s /opt/nvim/nvim.appimage /usr/bin/nvim
-else
-    warnings=$warnings"Could not symlink nvim.appimage to /usr/bin/nvim, /usr/bin/nvim already exists\n"
-fi
-
-if [ ! -f "/usr/local/bin/nvim"]; then
-    sudo ln -s /opt/nvim/nvim.appimage /usr/local/bin/nvim
-else
-    warnings=$warnings"Could not symlink nvim.appimage to /usr/local/bin/nvim, /usr/local/bin/nvim already exists\n"
-fi
-
+rm -rf ~/.local/share/nvim/
 git clone --depth 1 https://github.com/wbthomason/packer.nvim \
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 
-if [ -d "~/.config/nvim" ]; then
-    cp -r ~/.config/nvim backupDotfiles/
-fi
-ln -s "$REPO_DIR/nvim" ~/.config/
-
 sudo apt install python3-neovim npm nodejs fd-find ripgrep lolcat
-npm install -g neovim
+sudo npm install -g neovim
 
+
+
+PrintBlock "THE ERROR MESSAGE IS NORMAL JUST WAIT"
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
 
@@ -182,21 +221,29 @@ PrintBlock "Installing libinput-gestures"
 sudo apt install libinput-tools
 sudo gpasswd -a $USER input
 
-cd installer
 git clone https://github.com/bulletmark/libinput-gestures.git
 cd libinput-gestures
 sudo ./libinput-gestures-setup install
-if [ -f "~/.config/libinput-gestures.conf" ]; then
-    cp -r ~/.config/libinput-gestures.conf backupDotfiles/
+if [ -e ~/.config/libinput-gestures.conf ]; then
+    mv ~/.config/libinput-gestures.conf "$DOTFILES_DIR/backupDotfiles/"
 fi
-ln -s "$REPO_DIR/libinput-gestures.conf" ~/.config/
+ln -s "$DOTFILES_DIR/libinput-gestures.conf" ~/.config/
 
 libinput-gestures-setup autostart start
 cd ..
 rm -rf libinput-gestures
-cd ..
 
+
+
+
+#---------------End
+cd ..
+rm -rf installer
 
 PrintBlock "Finished"
 echo $warnings
-echo "Please restart yout copmputer to apply every changes"
+echo $'\n'
+echo "The installation is finished"
+echo "Your previous config files are stored inside the directory 'backupDotfiles'"
+echo "Please restart your computer to apply every changes"
+exit 0
