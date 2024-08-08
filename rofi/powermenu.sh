bf01f21471052b3ddc56e0b83513aaa360d04699
romain <rdoulaud@gmail.com>
2024-08-08 08:35:24 +0200

    feat: waybar

--- rofi/powermenu.sh
+++ rofi/powermenu.sh
@@ -7,20 +7,18 @@ rofi_command="rofi -no-config -theme $DIR/powermenu.rasi"
 
 # Options
 lock="  Lock"
-suspend="  Sleep"
 logout="󰗼  Logout"
 shutdown="⏻  Shutdown"
 reboot="  Restart"
 
 
 # Variable passed to rofi
-options="$lock\n$suspend\n$logout\n$reboot\n$shutdown"
+options="$lock\n$logout\n$reboot\n$shutdown"
 
 chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 0)"
 case $chosen in
-    $lock) bash ~/.config/i3/powermenu/lock.sh ;;
-    $suspend) systemctl suspend ;;
-    $logout) i3-msg exit ;;
+    $lock) hyprlock ;;
+    $logout) hyprctl dispatch exit ;;
     $shutdown) systemctl poweroff ;;
     $reboot) systemctl reboot ;;
 esac
